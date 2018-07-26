c816610201a5db2e9ee110b424d7b45509ecb495
natreed <natreed@pdx.edu>
2018-07-25 18:01:40 -0700

    finished yay

--- koans/src/java8/AboutBase64.java
+++ koans/src/java8/AboutBase64.java
@@ -21,7 +21,7 @@ public class AboutBase64 {
             // This uses the basic Base64 encoding scheme but there are corresponding
             // getMimeEncoder and getUrlEncoder methods available if you require a
             // different format/Base64 Alphabet 
-            assertEquals(encodedText, Base64.getEncoder().encodeToString(Byte.valueOf(encodedText)));
+            assertEquals(encodedText, Base64.getEncoder().encodeToString(plainText.getBytes("utf-8")));
         } catch (UnsupportedEncodingException ex) {}
     }
 
@@ -31,7 +31,7 @@ public class AboutBase64 {
         // This uses the basic Base64 decoding scheme but there are corresponding
         // getMimeDecoder and getUrlDecoder methods available if you require a
         // different format/Base64 Alphabet
-        byte[] decodedBytes = Base64.getDecoder().decode(__);
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedText);
         try {
             String decodedText = new String(decodedBytes, "utf-8");
             assertEquals(plainText, decodedText);
